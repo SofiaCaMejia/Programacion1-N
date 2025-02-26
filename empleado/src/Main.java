55529e15e1956c7e14c33ccec817b14a9a291810
SofiaCaMejia <sofia.cabezasm@uqvirtual.edu.co>
2025-02-25 23:27:29 -0500

    Add files via upload

---
+++ empleado/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Empleado empleado = new Empleado("Sofia","vendedora", "10049009", "sofia@uq.edu.co",
+                "23/03/2010", "312457410762");
+
+        System.out.println(empleado);
+
+    }
+}
